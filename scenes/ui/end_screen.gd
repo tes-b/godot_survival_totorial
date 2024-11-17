76b620e2521f4e0ffe2831efee9d1171ec50d8a2
tes-b <tsyi0313@gmail.com>
2024-11-17 23:36:42 +0900

    ui theme

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
+@onready var panel_container: PanelContainer = %PanelContainer
+
 
 func _ready() -> void:
+	panel_container.pivot_offset = panel_container.size / 2
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale",Vector2.ZERO, 0)
+	tween.chain()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .4)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	%RestartButton.pressed.connect(on_restart_button_pressed)
 	%QuitButton.pressed.connect(on_quit_button_pressed)	
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -4,19 +4,25 @@ signal upgrade_selected(upgrade:AbilityUpgrade)
 
 @export var upgrade_card_scene: PackedScene
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var card_container: HBoxContainer = $%CardContainer
 
 func _ready() -> void:
 	get_tree().paused = true
 	
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+		delay += .1
 		
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)	
+	animation_player.play("out")
+	await animation_player.animation_finished 
 	get_tree().paused = false
 	queue_free()
