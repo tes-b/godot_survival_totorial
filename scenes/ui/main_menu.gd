1749f0f7dc74df422fc59fd5964ae3ad10f91fab
tes-b <tsyi0313@gmail.com>
2025-02-18 21:12:02 +0900

    62.Creating a Main Menu

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$"%PlayButton".pressed.connect(on_play_pressed)
+	$"%OptionsButton".pressed.connect(on_options_pressed)
+	$"%QuitButton".pressed.connect(on_quit_pressed)
+
+
+func on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	
+func on_options_pressed():
+	pass
+
+func on_quit_pressed():
+	get_tree().quit()
