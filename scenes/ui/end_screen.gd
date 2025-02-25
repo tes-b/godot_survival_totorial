8dc2419200b29b77d6ab1458eefce070e867e12d
tes-b <tsyi0313@gmail.com>
2025-02-25 21:41:36 +0900

    66.Adding a scene transition Effect

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -30,6 +30,8 @@ func set_defeat():
 	
 	
 func on_restart_button_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 	
